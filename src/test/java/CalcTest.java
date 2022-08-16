9a5b8857bbf5a04ebaa6713e93dc04ede9b06d0e
Igor Cijov <cijovigor@yahoo.com>
2022-08-16 17:23:14 +0200

    Updated 16.08.2022

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CalcTest {
+    Calculator calculator;
+
+    @BeforeEach
+    public void init() {
+        calculator = new Calculator();
+    }
+
+    @CsvSource({
+            "1, 1, 2",
+            "2, 3, 5",
+            "7, 5, 12"
+    })
+
+    @ParameterizedTest
+    public void parameterizedTestAdd(int a, int b, int result){
+        Assertions.assertEquals(result, calculator.add(a,b));
+    }
+
+    @Test
+    public void testAdd () {
+        Assertions.assertEquals(4,calculator.add(2,2));
+    }
+
+    @Test
+    @Disabled
+    public void testSub(){
+        Assertions.assertEquals(-2,calculator.sub(2,4));
+
+    }
+}
